977973e41c4502d570a3718f97ad91a96fa9486b
DominicE <dominic.eubel@googlemail.com>
2014-08-21 19:17:55 +0200

    - added websocket library - added class apiManager

---
+++ App/src/main/java/de/trottl/staytment/apiManager.java
@@ -0,0 +1,10 @@
+package de.trottl.staytment;
+
+import android.os.AsyncTask;
+
+public class apiManager extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
