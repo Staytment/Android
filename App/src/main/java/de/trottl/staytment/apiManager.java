3bda33089e143539b72ce261f28d0ab0126e953e
DominicE <dominic.eubel@googlemail.com>
2014-08-21 19:51:44 +0200

    - added volley library - edited apiManager class

--- App/src/main/java/de/trottl/staytment/apiManager.java
+++ App/src/main/java/de/trottl/staytment/apiManager.java
@@ -1,10 +1,6 @@
 package de.trottl.staytment;
 
-import android.os.AsyncTask;
 
-public class apiManager extends AsyncTask {
-    @Override
-    protected Object doInBackground(Object[] objects) {
-        return null;
-    }
+
+public class apiManager {
 }
