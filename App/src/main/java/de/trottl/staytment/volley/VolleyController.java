71fb8857bec4a6b34b723dbdd24a50ceae00d7e0
DominicE <dominic.eubel@googlemail.com>
2014-08-25 18:13:56 +0200

    - removed en-strings - auto code optimization

--- App/src/main/java/de/trottl/staytment/volley/VolleyController.java
+++ App/src/main/java/de/trottl/staytment/volley/VolleyController.java
@@ -2,41 +2,36 @@ package de.trottl.staytment.volley;
 
 import android.app.Application;
 import android.text.TextUtils;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-/**
- * @author lmatzen
- */
-public class VolleyController extends Application
-{
-	public static final String TAG = VolleyController.class
-		.getSimpleName();
-
-	private RequestQueue mRequestQueue;
+public class VolleyController extends Application {
+    public static final String TAG = VolleyController.class
+            .getSimpleName();
+    private static VolleyController mInstance;
 //	private ImageLoader mImageLoader;
+    private RequestQueue mRequestQueue;
 
-	private static VolleyController mInstance;
+    public static synchronized VolleyController getInstance() {
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		mInstance = this;
-	}
+        return mInstance;
+    }
 
-	public static synchronized VolleyController getInstance() {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
 
-		return mInstance;
-	}
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
 
-	public RequestQueue getRequestQueue() {
-		if (mRequestQueue == null) {
-			mRequestQueue = Volley.newRequestQueue(getApplicationContext());
-		}
-
-		return mRequestQueue;
-	}
+        return mRequestQueue;
+    }
 
 //TODO add cache
 //	public ImageLoader getImageLoader() {
@@ -48,20 +43,20 @@ public class VolleyController extends Application
 //		return this.mImageLoader;
 //	}
 
-	public <T> void addToRequestQueue(Request<T> req, String tag) {
-		// set the default tag if tag is empty
-		req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-		getRequestQueue().add(req);
-	}
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
 
-	public <T> void addToRequestQueue(Request<T> req) {
-		req.setTag(TAG);
-		getRequestQueue().add(req);
-	}
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
 
-	public void cancelPendingRequests(Object tag) {
-		if (mRequestQueue != null) {
-			mRequestQueue.cancelAll(tag);
-		}
-	}
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
 }
